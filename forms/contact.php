3ecc6e8f92c6e3bd5ff332fd573f9d5077b62c2b
Henri-coder <66217494+Henri-coder@users.noreply.github.com>
2021-03-20 14:33:23 -0700

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,18 @@
-<?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+ <?php
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'pongenyengue48@gmail.com,nghotsopvianie@gmail.com';
+ if(isset($_POST["email"]) and isset($_POST["message"])){      
+  $email=$_POST["email"];   $message=$_POST["message"];
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+$header="MIME-Version: 1.0\r\n";
+$header.='Form:"Camer-Food.com"<support@Camer-Food.com>'."\n";
+$header.='content-type:text/html; charset="utf-8"'."\n";
+$header.='title:Camer-Food';
+$header.='Content-Transfer-Encoding: 8bit';
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+$message = str_replace("\n.", "\n..", $message);
+$email = $email.",pongenyengue48@gmail.com" ;
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  
-  $contact->smtp = array(
-    'host' => 'pongenyengue48@gmail.com,nghotsopvianie@gmail.com',
-    'username' => 'Kevin Vianie',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
-?>
+// send email
+mail($email,"OurProfiles.net",$message, $header);
+}
+?> 
